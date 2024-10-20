c6faeda12b00d91484fe0db7d8a05ec9d8ed7760
tobiadefami <tobiadefami@gmail.com>
2024-10-20 16:04:09 +0100

    fetch recent queries in type-ahead implementation

--- recommender/auto_complete.py
+++ recommender/auto_complete.py
@@ -1,19 +1,9 @@
-# TODO: implement a typeahead search component; it has to call an endpoint on the backend that provides autocompletions
+def autocomplete(query: str, existing_queries: list[str]) -> list[str]:
+    matching_queries = [
+        q
+        for q in existing_queries
+        if query.lower().strip() in q.lower().strip()
+    ]
 
-old_queries = [
-    "google pixel",
-    "iphone 12",
-    "iphone 13",
-    "iphone 16",
-    "macbook pro",
-    "macbook air",
-    "framework",
-]
-
-
-def autocomplete(query, queries_db=old_queries):
-    return [q for q in queries_db if query.lower().strip() in q.lower().strip()]
-
-
-if __name__ == "__main__":
-    print(autocomplete(" p "))
+    sorted_queries = sorted(matching_queries, key=lambda x: len(x))
+    return sorted_queries[:5]
